df2d306b8f7cba6befdb8724dafd6f36f463e907
fjg <18956662004@189.cn>
2020-09-11 09:33:18 +0800

    '初始化仓库完成'

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-10 09:20:16
+ * @LastEditTime: 2020-09-10 10:30:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \webpack-project\src\index.js
+ */
+// const name = "es6jxxx"
+var pro = new Promise
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-10 09:16:22
+ * @LastEditTime: 2020-09-11 09:23:47
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \webpack-project\webpack.config.js
+ */
+const path = require('path');
+const webpack = require('webpack');
+// const { watch } = require('less');
+
+module.exports = {
+//   watch: true,
+  mode:'development',
+  entry: './src/index.js',
+  output: {
+      path: path.resolve(__dirname, 'src'),
+      filename: 'bundle.js'
+      } ,
+  module:{
+      rules:[{ test: /\.js$/, use: 'babel-loader' },
+      {test: /\.css$/, use: ['style-loader','css-loader']},
+      { 
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: 'file-loader' 
+    },
+    { 
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader' 
+    } ]
+  },
+  plugins:[
+      new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer:{
+    host:'localhost',
+    port:'8080',
+    open:true,  //自动拉起浏览器
+    hot:true,   //热加载
+    contentBase:'./dist' // 内容目录
+  }
+  
+};
